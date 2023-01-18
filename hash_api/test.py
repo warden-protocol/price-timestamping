9092624ee77aa217fcc8054d5ab56b14192d2080
Qredo LTD <support@qredo.com>
2023-01-18 13:55:52 +0100

    interim save

--- hash_api/test.py
+++ hash_api/test.py
@@ -10,38 +10,31 @@ in_target='USD'
 
 
 #load env
-load_dotenv('../../../../../../../../../home/ubuntu/price-timestamping/.env')
+load_dotenv('//home/ubuntu/price-timestamping/.env')
 ubuntu_pw = os.getenv('ubuntu_pw')
 #cargo 
 cargo_run = 'cargo run --manifest-path Cargo.toml . proof_output/ '+in_hash+' 0 '+'price_data/data/'+in_ts+'/'+in_symbol+'_'+in_target+'.json'
-cargo_output = 'git show '+in_hash+'~0:price_data/data/'+in_ts+'/'+in_symbol+'_'+in_target+'.json > ../../../../../../../../../home/ubuntu/price-timestamping/proof_output/res'
-#JUST ECHO TO OUTPOUTFILE
-#
-oldterm = os.environ['TERM']
-os.environ['TERM'] = 'xterm'
+cargo_output = 'git show '+in_hash+'~0:price_data/data/'+in_ts+'/'+in_symbol+'_'+in_target+'.json > //home/ubuntu/price-timestamping/proof_output/res'
 
 #switch to ubuntu user
 child = pexpect.spawn('su - ubuntu',encoding='utf-8')
 child.sendline(ubuntu_pw)
 child.expect('\$')
 #run cargo routine
-child.sendline('cd ../../../../../../../../../home/ubuntu/price-timestamping/')
+child.sendline('cd //home/ubuntu/price-timestamping/')
 child.expect('\$')
 child.sendline(cargo_run)
 child.expect('\$')
 child.sendline('cd /proof_output/')
 child.expect('\$')
-#child.sendline('touch ../../../../../../../../../home/ubuntu/price-timestamping/res')
-
-#child.logfile = open("../../../../../../../../../home/ubuntu/price-timestamping/res", "w")
 child.sendline(cargo_output)
 child.expect('\$')
+#read in
+with open('/home/ubuntu/price-timestamping/proof_output/res') as f:
+    res = f.readlines()
 
-child.before
-child.close()
-
-res = str(child.before).replace("\r","").replace("\n","").split('{prices_json:')[1].split('}}')[0]
-
+child.sendline('yes | rm -r /home/ubuntu/price-timestamping/proof_output/')
+child.expect('\$')
 #yes | rm -r proof_output
 #rm mylog.txt
 

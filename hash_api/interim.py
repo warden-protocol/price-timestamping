4dac7704279657a93f50264d0360625fe5f96183
Qredo LTD <support@qredo.com>
2023-01-24 15:41:42 +0100

    working example but needs debuggin

--- hash_api/interim.py
+++ hash_api/interim.py
@@ -35,22 +35,21 @@ def getprices_hour(target:str='USD',symbol:str='QRDO',ts:str='2023-01-04-14',has
     os.chdir('price-timestamping')
     os.system('git pull https://oauth2:'+gitlab_token+'@gitlab.qredo.com/data_analytics/price-timestamping.git')
 
-    cargo_run='cargo run --manifest-path Cargo.toml . proof_output/ aeb690ac7c9860cdf909c93351c2742298378791 0 price_data/data/2023-01-04-14/QRDO_USD.json'
-    cargo_output='git show aeb690ac7c9860cdf909c93351c2742298378791~0:price_data/data/2023-01-04-14/QRDO_USD.json > /usr/src/app/price-timestamping/proof_output/res'
-    # cargo_run = 'cargo run --manifest-path Cargo.toml . proof_output/ '+hash+' 0 '+'price_data/data/'+ts+'/'+symbol+'_'+target+'.json'
-    # cargo_output = 'git show '+hash+'~0:price_data/data/'+ts+'/'+symbol+'_'+target+'.json > ' +os.getcwd()+ 'proof_output/res'
+    #cargo_run='cargo run --manifest-path Cargo.toml . proof_output/ aeb690ac7c9860cdf909c93351c2742298378791 0 price_data/data/2023-01-04-14/QRDO_USD.json'
+    #cargo_output='git show aeb690ac7c9860cdf909c93351c2742298378791~0:price_data/data/2023-01-04-14/QRDO_USD.json > /usr/src/app/price-timestamping/proof_output/res'
+    cargo_run = 'cargo run --manifest-path Cargo.toml . proof_output/ '+hash+' 0 '+'price_data/data/'+ts+'/'+symbol+'_'+target+'.json'
+    cargo_output = 'git show '+hash+'~0:price_data/data/'+ts+'/'+symbol+'_'+target+'.json > ' +os.getcwd()+ '/proof_output/res'
 
     os.system(cargo_run)
-    os.chdir('proof_output/')
     os.system(cargo_output)
     # #read in
-    with open('res') as f:
+    with open('proof_output/res') as f:
         res = f.read().splitlines()
     res = str(res[0]).replace('prices_json','"prices_json"') #fix later^^
     res = json.dumps(res, indent=4, default=str)
     
     # os.system('yes | rm -r ../proof_output/')
-    os.chdir('../../')
+    os.chdir('../')
     return Response(content=res,media_type="application/json")
 
 # https://stackoverflow.com/questions/32163955/how-to-run-shell-script-on-host-from-docker-container

fbe397790be6de1f3379dcafdf61ca6278880c42
Qredo LTD <support@qredo.com>
2023-01-31 12:30:18 +0100

    interim save

--- etl_pricedumping/pricedumping.py
+++ etl_pricedumping/pricedumping.py
@@ -2,11 +2,18 @@ from dotenv import load_dotenv
 import os
 import json
 import requests
+from web3 import Web3
 
 #get env
 load_dotenv('price-timestamping/.env')
 gitlab_token = os.getenv('gitlab_token')
 gitlab_email = os.getenv('gitlab_email')
+infura_url = os.getenv('infura_url')
+account_sender = os.getenv('account_sender')
+account_receiver = os.getenv('account_receiver')
+pkey_sender = os.getenv('pkey_sender')
+
+
 #add git configuration for docker container
 os.system('git config --global user.email "'+gitlab_email+'"') 
 os.system("git config --global --add safe.directory '*'")
@@ -27,12 +34,36 @@ else:
 r = requests.get('http://data.dev.qredo.loc:5501/prices_hour_all?nlast=1')
 r = json.loads(r.text)
 
-if not os.path.isdir('sub/price-timestamping/'+str(r[0]['price_ts'])):
+if not os.path.isdir('sub/price-timestamping/price_data/data/'+str(r[0]['price_ts'])):
     print(r)
-    os.system('mkdir '+'sub/price-timestamping/'+str(r[0]['price_ts']))
-    os.system('echo '+json.dumps(r)+' >'+'sub/price-timestamping/'+str(r[0]['price_ts'])+'/'+str(r[0]['symbol'])+'_'+str(r[0]['target'])+'.json')
+    os.system('mkdir '+'sub/price-timestamping/price_data/data/'+str(r[0]['price_ts']))
+    os.system('echo '+json.dumps(r)+' >'+'sub/price-timestamping/price_data/data/'+str(r[0]['price_ts'])+'/'+str(r[0]['symbol'])+'_'+str(r[0]['target'])+'.json')
     os.system('git -C sub/price-timestamping/ add .')
     os.system('git -C sub/price-timestamping/ commit -m "autocommit pricedata" ')
     print('committed')
     #os.system('git -C sub/price-timestamping/ push --set-upstream origin price-dumping-branch https://oauth2:' + gitlab_token + '@gitlab.qredo.com/data_analytics/price-timestamping.git')
     os.system('git -C sub/price-timestamping/ push https://oauth2:' + gitlab_token + '@gitlab.qredo.com/data_analytics/price-timestamping.git')
+    git_hash = os.popen('git -C sub/price-timestamping/  rev-parse HEAD').read().splitlines()[0].strip()
+    web3 = Web3(Web3.HTTPProvider(infura_url))
+    nonce = web3.eth.getTransactionCount(account_sender)
+
+    tx = {
+    'nonce': nonce,
+    'to': account_receiver,
+    'value': web3.toWei(0.00001, 'ether'),
+    'gas': 2000000,
+    'gasPrice': web3.toWei('50', 'gwei'),
+    'data': bytes('proof of commit hash: '+git_hash,'utf8')
+    }
+
+    signed_tx = web3.eth.account.sign_transaction(tx, pkey_sender)
+    tx_hash = web3.eth.sendRawTransaction(signed_tx.rawTransaction)
+
+    os.system('mkdir '+'sub/price-timestamping/price_data/proofs/'+str(r[0]['price_ts']))
+
+    proof = {
+    "tx_hash": tx_hash,
+    "commit_hash": git_hash
+    }
+
+    os.system('echo '+json.dumps(proof)+' >'+'sub/price-timestamping/price_data/proofs/'+str(r[0]['price_ts'])+'/'+'proof.json')
